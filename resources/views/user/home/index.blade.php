ef9c04d945b6ba151c03367b831680a532a2f729
yogameleniawan <yogameleniawan@gmail.com>
2022-03-22 18:30:50 +0700

    update home page catalog

---
+++ resources/views/user/home/index.blade.php
@@ -0,0 +1,221 @@
+@extends('user.layouts.app')
+@section('css')
+    <style>
+        .form-control {
+            padding: 1.1rem 0.75rem;
+        }
+        .form-group[class*=has-icon-].has-icon-right .form-control {
+            padding-right: 2.5rem;
+            border-radius: 10px;
+        }
+
+        .banner-img{
+            width:100%;filter: brightness(0.3);
+        }
+
+        .centered {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        .img-container {
+            position: relative;
+            text-align: center;
+            color: white;
+        }
+
+        .card-position{
+            position: relative;
+            margin-top: -10%;
+            width: 90%;
+            margin-left: 5%;
+        }
+
+        .row-card {
+            display: flex;
+        }
+
+        .col-6 {
+            flex: 0 0 auto;
+            width: 50%;
+            margin-right: 10px;
+        }
+
+        .card-head .card-body-head {
+            /* padding: 1.5rem; */
+        }
+
+        .card-body-head {
+            flex: 1 1 auto;
+            padding: 0.8rem;
+            background-color: white;
+            border-radius: 15px;
+        }
+
+        .total-product{
+            background-color: #4fbe87;
+            text-align: center;
+            border-radius: 10px;
+            color: white;
+        }
+    </style>
+@endsection
+@section('page-heading')
+    <div class="col-12 img-container">
+        <img src="{{url('img/banner.jpeg')}}" class="banner-img">
+        <div class="centered">Selamat datang, <br> <small style="font-size:20px">User!</small></div>
+    </div>
+
+    <div class="row-card card-position" >
+        <div class="col-6">
+            <div class="card-head">
+                <div class="card-body-head">
+                    <div class="row">
+                        <div class="col-12">
+                            <b>Produk</b><br>
+                            <p>Pesanan Kamu</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-6">
+                                    <p>Jumlah </p>
+                                </div>
+                                <div class="col-5">
+                                    <p class="total-product">3</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card-head">
+                <div class="card-body-head">
+                    <div class="row">
+                        <div class="col-12">
+                            <b>Produk</b><br>
+                            <p>Pesanan Kamu</p>
+                        </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-6">
+                                    <p>Jumlah : </p>
+                                </div>
+                                <div class="col-5">
+                                    <p>3</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<div class="form-group position-relative has-icon-right m-3">
+    <input type="text" class="form-control" placeholder="Cari Produk ... ">
+    <div class="form-control-icon">
+        <i class="bi bi-search"></i>
+    </div>
+</div>
+
+@endsection
+@section('content')
+<div class="row">
+    <div class="col-12 col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4"><img src="{{url('img/martabak.jpg')}}" class="img-product"/></div>
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-12">
+                                <small>Martabak</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p><b>Rp. 25.000,0</b></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8 add-to-cart">
+                                <b><i class="bi bi-cart-fill"></i> Pesan</b>
+                            </div>
+                            <div class="col-4">
+                                <b>[]</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12 col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4"><img src="{{url('img/martabak.jpg')}}" class="img-product"/></div>
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-12">
+                                <small>Martabak</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p><b>Rp. 25.000,0</b></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8 add-to-cart">
+                                <b><i class="bi bi-cart-fill"></i> Pesan</b>
+                            </div>
+                            <div class="col-4">
+                                <b>[]</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12 col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4"><img src="{{url('img/martabak.jpg')}}" class="img-product"/></div>
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-12">
+                                <small>Martabak</small>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p><b>Rp. 25.000,0</b></p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-8 add-to-cart">
+                                <b><i class="bi bi-cart-fill"></i> Pesan</b>
+                            </div>
+                            <div class="col-4">
+                                <b>[]</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/user/layouts/app.blade.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>TOKO | NB PRODUCTION</title>
+    <!-- Favicon-->
+    <link rel="icon" href="{{ url('admin/images/favicon.png') }}" type="image/x-icon">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('mazer/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/iconly/bold.css') }}">
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/choices.js/choices.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ url('mazer/assets/css/app.css') }}">
+    <link rel="shortcut icon" href="{{ url('mazer/assets/images/favicon.svg') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/simple-datatables/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="{{ url('catalog/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ url('css/ppdb.css') }}">
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/summernote/summernote-lite.min.css') }}">
+    <link rel="stylesheet" href="{{ url('mazer/assets/vendors/toastify/toastify.css') }}">
+    <link rel="stylesheet" href="{{ url('catalog/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ url('catalog/css/mobile-view.css') }}">
+
+    {{-- <link rel="stylesheet" type="text/css"  href="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"> --}}
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.min.css">
+    {{-- <link rel="stylesheet" type="text/css"  href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css"> --}}
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.js">
+
+    @yield('css')
+    <style>
+        .page-item.active .page-link {
+            z-index: 3;
+            color: #fff;
+            background-color: #ea342a;
+            border-color: #ea342a;
+        }
+
+        .sidebar-wrapper .sidebar-header img {
+            height: 5.299999999999997rem;
+        }
+
+        .sidebar-wrapper {
+            width: 300px;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            z-index: 10;
+            overflow-y: auto;
+            background-color: #272727;
+            bottom: 0;
+            transition: left .5s ease-out;
+        }
+
+        .sidebar-wrapper .menu .sidebar-link {
+            display: block;
+            padding: 0.7rem 1rem;
+            font-size: 1rem;
+            display: flex;
+            align-items: center;
+            border-radius: 0.5rem;
+            transition: all .5s;
+            text-decoration: none;
+            color: #ffffff;
+        }
+
+        .sidebar-wrapper .menu .sidebar-link i, .sidebar-wrapper .menu .sidebar-link svg {
+            color: #857f3d;
+        }
+
+        a {
+            color: #857f3d;
+            text-decoration: underline;
+        }
+
+        .navbar {
+            height: 90px;
+            padding: 1.5rem;
+            background-color: #272727;
+        }
+
+        #main #main-content {
+            padding: 1rem;
+        }
+
+        .img-product{
+            width:100%;
+            border-radius: 10px;
+        }
+
+        .add-to-cart{
+            background-color: #4fbe87;
+            border-radius: 10px;
+            color: white;
+            text-align: -webkit-center;
+            padding: 5px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img src="{{ url('img/logo.jpeg') }}"></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+
+                        <li class="sidebar-item ">
+                            <a href="" class='sidebar-link'>
+                                <i class="bi bi-house-fill"></i>
+                                <span>
+                                    PRODUK
+                                </span>
+                            </a>
+                        </li>
+                        {{-- <li
+                            class="sidebar-item has-sub {{ Route::is('program.create') || Route::is('program.index') || Route::is('history.index') ? 'active' : '' }}">
+                            <a href="javascript:void(0);" class='sidebar-link'>
+                                <i class="bi bi-book-half"></i>
+                                <span>
+                                    {{ __('student.menu.application') }}
+                                </span>
+                            </a>
+                            <ul
+                                class="submenu {{ Route::is('program.create') || Route::is('program.index') || Route::is('history.index') ? 'active' : '' }}">
+                                <li
+                                    class="submenu-item {{ Route::is('program.create') || Route::is('program.index') ? 'active' : '' }}">
+                                    <a href="{{ route('program.index') }}">
+                                        {{ __('student.menu.startnewapplication') }}
+                                    </a>
+                                </li>
+                                <li class="submenu-item {{ Route::is('history.index') ? 'active' : '' }}">
+                                    <a href="{{ route('history.index') }}">
+                                        {{ __('student.menu.applyhistory') }}
+                                    </a>
+                                </li>
+                            </ul>
+                        </li> --}}
+
+                        <li class="sidebar-item  ">
+                            <a href="" class='sidebar-link'>
+                                <i class="bi bi-credit-card-2-back-fill"></i>
+                                <span>
+                                    PESANAN
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
+        <div id="main" class='layout-navbar'>
+            <header>
+                <nav class="navbar navbar-expand navbar-light ">
+                    <div class="container-fluid">
+                        <a href="#" class="burger-btn d-block">
+                            <i class="bi bi-justify fs-3"></i>
+                        </a>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                            </ul>
+                            <div class="dropdown">
+                                <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="user-menu d-flex">
+                                        <div class="user-name text-end me-3">
+                                            <h6 class="mb-0 text-gray-600">
+
+                                            </h6>
+                                            <p class="mb-0 text-sm text-gray-600">
+
+                                            </p>
+                                        </div>
+                                        <div class="user-img d-flex align-items-center">
+                                            <div class="avatar avatar-md">
+                                                <img
+                                                src="{{url('mazer/assets/images/faces/1.jpg') }}">
+                                           </div>
+                                        </div>
+                                    </div>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                                    <li>
+                                        <h6 class="dropdown-header">Halo,
+
+                                        </h6>
+                                    </li>
+
+                                    <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();" role="button">
+                                            <i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                                        Keluar
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+            @yield('page-heading')
+            <div id="main-content">
+
+                <div class="page-heading">
+
+                    @yield('content')
+                </div>
+
+                <footer>
+                    <div class="footer clearfix mb-0 text-muted">
+                        <div class="float-start">
+                            <p>2022 &copy; NB Production</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+    <script src="{{ url('mazer/assets/vendors/choices.js/choices.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js"
+        integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    <script src="{{ url('mazer/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ url('mazer/assets/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ url('mazer/assets/js/main.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+
+    <script src="{{ url('mazer/assets/vendors/toastify/toastify.js') }}"></script>
+    <script src="{{ url('mazer/assets/js/extensions/toastify.js') }}"></script>
+
+    <script src="{{ url('mazer/assets/vendors/summernote/summernote-lite.min.js') }}"></script>
+    <script>
+        $('#summernote').summernote({
+            tabsize: 2,
+            height: 120,
+        })
+
+        $('#summernote1').summernote({
+            tabsize: 2,
+            height: 120,
+        })
+    </script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/61dcedd4f7cf527e84d1726f/1fp3g21tq';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+
+    @yield('script')
+</body>
+
+</html>
